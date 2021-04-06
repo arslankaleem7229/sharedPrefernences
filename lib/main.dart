aaf670f6dab17c05a2d2ad3312df638e4c7b7236
arslankaleem7229 <arslankaleem7229@gmail.com>
2021-04-06 18:38:59 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:path_provider/path_provider.dart';
 
 void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SharedPreferences(),
-      debugShowCheckedModeBanner: false,
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _savedData = "";
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedData();
   }
-}
 
-class SharedPreferences extends StatefulWidget {
-  @override
-  _SharedPreferencesState createState() => _SharedPreferencesState();
-}
+  _loadSavedData() async {
+    SharedPreferences _preferences = await SharedPreferences.getInstance();
+    setState(() {
+      if (_preferences.getString("data").isNotEmpty &&
+          _preferences.getString("data") != null) {
+        _savedData = _preferences.getString("data");
+      } else {
+        _savedData = "Empty";
+      }
+    });
+  }
 
-class _SharedPreferencesState extends State<SharedPreferences> {
+  _saveMessage(String message) async {
+    SharedPreferences _preferences = await SharedPreferences.getInstance();
+    _preferences.setString("data", message);
+  }
+
   TextEditingController _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Shared Preferences"),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrange,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(13.4),
-        alignment: Alignment.topCenter,
-        child: ListTile(
-          title: TextField(
-            controller: _textEditingController,
-            decoration: InputDecoration(labelText: "Write something"),
-          ),
-          subtitle: TextButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Shared Preferences"),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(13.4),
+          alignment: Alignment.topCenter,
+          child: ListTile(
+            title: TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(labelText: "Write something"),
             ),
-            child: Column(
-              children: [
-                Text("Save Data"),
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                ),
-              ],
+            subtitle: TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  new Text("Save Data"),
+                  new Padding(padding: EdgeInsets.all(15.0)),
+                  new Text(_savedData),
+                ],
+              ),
+              onPressed: () {
+                _saveMessage(_textEditingController.text);
+              },
             ),
-            onPressed: () {},
           ),
         ),
       ),
     );
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _textEditingController.dispose();
+  }
 }
-
-// Future<String> get _localpath async {
-//   final directory = await getApplicationDocumentsDirectory();
-//   return directory.path;
-// }
-
-// Future<File> get _localfile async {
-//   final path = await _localpath;
-//   return new File("$path/data.txt");
-// }
-
-// Future<File> writeData(String message) async {
-//   final file = await _localfile;
-//   return file.writeAsString("$message");
-// }
-
-// Future<String> readData() async {
-//   try {
-//     final file = await _localfile;
-//     final String data = await file.readAsString();
-//     return data;
-//   } on Exception catch (e) {
-//     print("$e: File not found or failed to fetch data!");
-//     return null;
-//   }
-// }
